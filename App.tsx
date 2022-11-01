d4d73a593f488d78ef400e144126a63e2a2528b5
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-11-01 16:40:20 -0300

    Aula 01 - Iniciando projeto de ponta a ponta

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { NativeBaseProvider, StatusBar } from 'native-base'
+
+import { THEME } from './src/styles/theme'
+
+import { Loading } from './src/components/Loading'
+import { Signin } from './src/screens/Signin'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  })
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Signin /> : <Loading />}
+    </NativeBaseProvider>
+  )
+}
